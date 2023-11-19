c415f9fe00634b769b1a2b0e7bb8d99ecd7899cd
rushikeshghuge <rushikeshghuge993@gmail.com>
2023-11-19 10:56:48 +0530

    done the to do project

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,42 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [taskList, setTaskList] = useState([]);
+  const [task, setTask] = useState('');
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <div className="App">
+      {/* Do not remove the main div */}
+      <h1 style={{ textAlign: 'center' }}>To-Do List</h1>
+      <div style={{
+        textAlign: 'center'
+      }}>
+        <input value={task} onChange={(e) => {
+          setTask(e.target.value)
+        }} type="text" />
+        <button onClick={() => {
+          setTaskList([...taskList, task]);
+          setTask('');
+        }}>Add Todo</button>
+      </div>
+
+      <div className="todoList">
+        {taskList.map((task, index) => {
+          return <div key={index} className="todoItem">
+            <p> {task} </p> <button onClick={() => {
+              {
+                setTaskList(taskList.filter((item, i) => {
+                  return index !== i;
+                }))
+              }
+            }}>Delete</button>
+          </div>
+        })}
+      </div>
+    </div >
   )
 }
 
